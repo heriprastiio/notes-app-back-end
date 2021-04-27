c75a86ede64b0a61288fcd087f06e1dcc5d484c0
heriprastiio <heri.prastio56@gmail.com>
2021-04-27 16:54:27 +0700

    Query Parameters

---
+++ configserverqueryparameter.js
@@ -0,0 +1,24 @@
+const configRoutes = [
+  {
+    method: 'GET',
+    path: '/',
+    handler: (request, h) => {
+      const { name, location } = request.query;
+      return `Hello, ${name} from ${location}`;
+    },
+  },
+  {
+    method: 'GET',
+    path: '/hello/{name?}',
+    handler: (request, h) => {
+      const { name = 'spongebob' } = request.params;
+      const { lang } = request.query;
+      if (lang === 'id'){
+        return `Hai, ${name}!`;
+      }
+      return `Hello, ${name}!`;
+    },
+  },
+];
+
+module.exports = configRoutes;
