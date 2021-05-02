e3b752a299a33fe912489a223fd2f2ca84887830
heriprastiio <heri.prastio56@gmail.com>
2021-05-02 21:32:09 +0700

    Fix bugs host

--- configserverqueryparameter.js
+++ configserverqueryparameter.js
@@ -9,11 +9,18 @@ const configRoutes = [
   },
   {
     method: 'GET',
+    path: '/',
+    handler: (request, h) => {
+      request.end('Hello World');
+    },
+  },
+  {
+    method: 'GET',
     path: '/hello/{name?}',
     handler: (request, h) => {
       const { name = 'spongebob' } = request.params;
       const { lang } = request.query;
-      if (lang === 'id'){
+      if (lang === 'id') {
         return `Hai, ${name}!`;
       }
       return `Hello, ${name}!`;
